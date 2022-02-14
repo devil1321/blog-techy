28e6c4d17f9884eef521209babfaa3d6ed1fa658
Dominik Stepien <s.dominik1321@gmail.com>
2022-02-14 16:12:10 +0100

    init tempates

---
+++ src/templates/blog.template.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Seo from '../components/seo.component'
+import Layout from '../components/layout.component'
+
+const Blog = () => {
+  return (
+      <div class="blog-page">
+         <Seo title ="Technology | Blog" /> 
+      </div>
+  )
+}
+
+export default Blog
