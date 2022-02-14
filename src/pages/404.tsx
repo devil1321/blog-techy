e0ade1d1726643d29aa75d4d33b8fa96c98b95c2
Dominik Stepien <s.dominik1321@gmail.com>
2022-02-14 16:06:27 +0100

    init pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout.component'
+const NotFound = () => {
+  return (
+    <Layout>
+        <div>NotFound</div>
+    </Layout>
+  )
+}
+
+export default NotFound
