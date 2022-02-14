e24ea02cdd69604e5a8e8e17d5bf2020b24bd70d
Dominik Stepien <s.dominik1321@gmail.com>
2022-02-14 17:43:30 +0100

    fix

--- src/templates/author.template.tsx
+++ src/templates/author.template.tsx
@@ -5,15 +5,7 @@ const Author = () => {
   return (
       <div class="author-page">
          <Seo title ="Technology | Author" /> 
-<<<<<<< HEAD
-<<<<<<< HEAD
-         author page
-=======
-         auth templates
->>>>>>> a0fe35abb1eb76dcfd9a61a189168347e41fc9a0
-=======
          author page test
->>>>>>> 18ed4e92749ded1ded090b4426c7450a0c3aead6
       </div>
   )
 }
