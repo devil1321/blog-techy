75e793999bb4a46d8bdc0d9d307a48c9c1910859
Dominik Stepien <s.dominik1321@gmail.com>
2022-02-16 12:00:46 +0100

    update pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { PageQueryArticlesArr,PageQueryArticles } from '../interfaces'
+import Seo from '../components/seo.component'
+import Layout from '../components/layout.component'
+import BlogItem from '../components/blog-item.component'
+const Home:React.FC<PageQueryArticlesArr> = ({data}):JSX.Element => {
+  return (
+    <Layout>
+      <div className="about-page">
+        <Seo title ="Technology | About Us" />
+        <h1>Gaming Posts</h1>
+        {data.allContentfulArticles.nodes.map((node:PageQueryArticles)=>{
+          return <BlogItem key={node.id} data={node} />
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+
+export const query = graphql`
+  {
+    allContentfulArticles(filter: {category: {eq: "gaming"}}) {
+      nodes {
+        id
+        contentfulid
+        category
+        title
+        subtitle
+        url
+        date(formatString: "")
+        img {
+          gatsbyImageData(layout: CONSTRAINED, formats: WEBP, placeholder: BLURRED)
+        }
+        author {
+          id
+          authorName
+          authorEmail
+          dateOfBirth
+          userImage {
+            gatsbyImageData(layout: CONSTRAINED, formats: WEBP, placeholder: BLURRED)
+            description
+            title
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Home
