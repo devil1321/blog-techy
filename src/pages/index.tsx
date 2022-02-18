334877d512fc8d8a5ccd562aaa41d1984d60ca19
Dominik Stepien <s.dominik1321@gmail.com>
2022-02-18 19:27:57 +0100

    small fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ const All:React.FC<PageQueryArticlesArr> = (props):JSX.Element => {
           <h2 className="title">Intresting people around world</h2>
           <div className="about-page__about-us-content">
            <div className="about-page__about-us-text">
-             <h2 className="title">Meet new <span className="purple">people</span> and develop <span className="purple">passions</span></h2>
+             <h2 className="title">Meet <span className="purple">new</span> people and develop <span className="purple">passions</span></h2>
              <p>Our team consists of creative people who know their field. Join now and make an appointment</p>
            </div>
            <div className="about-page__about-us-img">
