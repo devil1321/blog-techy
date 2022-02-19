d14a7396bdce8c280016cfbff38746e350cc8cd1
Dominik Stepien <s.dominik1321@gmail.com>
2022-02-19 14:45:30 +0100

    site finished ready to check

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'gatsby'
 import React from 'react'
 import LayoutWithAside from '../components/layout-with-aside.component'
 const NotFound = () => {
   return (
     <LayoutWithAside>
-        <div>NotFound</div>
+        <div className="not-found">
+          <div className="not-found__content">
+            <h1>Page Not Found</h1>
+            <p>We cannot find this page</p>
+            <Link to="/">Back to home</Link>
+          </div>
+        </div>
     </LayoutWithAside>
   )
 }
